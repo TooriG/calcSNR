6656ae2c1b776d11f8591b2f22ef2e1a9b0c8402
TooriG <97956577+TooriG@users.noreply.github.com>
2023-09-22 12:59:17 +0900

    Update show_pnginfo.py

--- show_pnginfo.py
+++ show_pnginfo.py
@@ -26,37 +26,34 @@ st.title("PNG Metadata Extractor for Multiple Images")
 uploaded_files = st.file_uploader("複数のPNG画像をアップロードしてください", type=["png"], accept_multiple_files=True)
 
 if uploaded_files:
-    all_images_lack_metadata = True
+    all_missing_metadata = True  # すべての画像にメタデータがないかどうかをチェックするフラグ
+
     for uploaded_file in uploaded_files:
         metadata = extract_png_info(uploaded_file)
         if 'parameters' in metadata:
-            all_images_lack_metadata = False
+            all_missing_metadata = False
             break
 
-    if all_images_lack_metadata:
-        st.error("すべての画像にメタデータが存在しません。")
+    if all_missing_metadata:
+        st.error("すべての画像にメタデータが存在しません")
 
-    else:
-        zip_buffer = io.BytesIO()
-        with zipfile.ZipFile(zip_buffer, 'a', zipfile.ZIP_DEFLATED) as z:
-            for uploaded_file in uploaded_files:
-                image_bytes = remove_metadata_and_get_bytes(uploaded_file)
-                z.writestr(uploaded_file.name, image_bytes)
-        zip_buffer.seek(0)
-        st.download_button("メタデータを削除した画像を一括ダウンロード", zip_buffer, file_name="images_without_metadata.zip", mime="application/zip")
+    # 一括ダウンロード用のZIPファイルを作成
+    zip_buffer = io.BytesIO()
+    with zipfile.ZipFile(zip_buffer, 'a', zipfile.ZIP_DEFLATED) as z:
+        for uploaded_file in uploaded_files:
+            image_bytes = remove_metadata_and_get_bytes(uploaded_file)
+            z.writestr(uploaded_file.name, image_bytes)
+    zip_buffer.seek(0)
+    st.download_button("メタデータを削除した画像を一括ダウンロード", zip_buffer, file_name="images_without_metadata.zip", mime="application/zip")
     
-    for i in range(0, len(uploaded_files), 2):
-        col1, col2 = st.columns(2)
-        
-        with col1:
-            metadata1 = extract_png_info(uploaded_files[i])
-            st.image(uploaded_files[i], caption="Uploaded Image.", use_column_width=True)
-            if 'parameters' in metadata1:
-                st.text_area("", value=metadata1['parameters'], height=200, max_chars=None)
-
-        if i + 1 < len(uploaded_files):  # 画像が奇数の場合、最後の列を確認
-            with col2:
-                metadata2 = extract_png_info(uploaded_files[i + 1])
-                st.image(uploaded_files[i + 1], caption="Uploaded Image.", use_column_width=True)
-                if 'parameters' in metadata2:
-                    st.text_area("", value=metadata2['parameters'], height=200, max_chars=None)
+    # 画像とメタデータを6列で表示
+    for idx, uploaded_file in enumerate(uploaded_files):
+        cols = st.columns(6)
+        col = cols[idx % 6]
+        with col:
+            metadata = extract_png_info(uploaded_file)
+            col.image(uploaded_file, caption="Uploaded Image.", use_column_width=True)
+            
+            if 'parameters' in metadata:
+                parameters = metadata['parameters']
+                col.text_area("", value=parameters, height=200, max_chars=None)
