737cae433a95881fb1aa26220ae3d1f8dbb5176c
TooriG <97956577+TooriG@users.noreply.github.com>
2023-09-22 12:45:17 +0900

    Update show_pnginfo.py

--- show_pnginfo.py
+++ show_pnginfo.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from PIL import Image
 import io
+import zipfile
 
 def extract_png_info(image_path):
     """PNG画像からメタデータを抽出する関数"""
@@ -11,54 +12,38 @@ def extract_png_info(image_path):
 def remove_metadata_and_get_bytes(image_path):
     """画像からメタデータを削除して、バイトデータを返す関数"""
     with Image.open(image_path) as img:
-        # メタデータを削除
         data = list(img.getdata())
         img_without_metadata = Image.new(img.mode, img.size)
         img_without_metadata.putdata(data)
-        
-        # 画像をバイトデータとして保存
         byte_io = io.BytesIO()
         img_without_metadata.save(byte_io, format="PNG")
     return byte_io.getvalue()
 
-st.title("PNG Metadata Extractor")
+st.set_page_config(layout="wide")  # UIの横幅を広く取る
 
-uploaded_file = st.file_uploader("PNG画像をアップロードしてください", type=["png"])
+st.title("PNG Metadata Extractor for Multiple Images")
 
-if uploaded_file:
-    with st.spinner("メタデータを抽出中..."):
+uploaded_files = st.file_uploader("複数のPNG画像をアップロードしてください", type=["png"], accept_multiple_files=True)
+
+if uploaded_files:
+    # 一括ダウンロード用のZIPファイルを作成
+    zip_buffer = io.BytesIO()
+    with zipfile.ZipFile(zip_buffer, 'a', zipfile.ZIP_DEFLATED) as z:
+        for uploaded_file in uploaded_files:
+            image_bytes = remove_metadata_and_get_bytes(uploaded_file)
+            z.writestr(uploaded_file.name, image_bytes)
+    zip_buffer.seek(0)
+    st.download_button("メタデータを削除した画像を一括ダウンロード", zip_buffer, file_name="images_without_metadata.zip", mime="application/zip")
+    
+    for uploaded_file in uploaded_files:
         metadata = extract_png_info(uploaded_file)
-
-        if 'parameters' in metadata:
-            parameters = metadata['parameters']
-            st.text_area("Parameters:", value=parameters, height=400)
-            st.write("上のテキストをコピーしてください")
-            
-        if "comment" in metadata:
-            st.subheader("Comment")
-            st.write(metadata["comment"])
+        col1, col2 = st.columns([1, 2])
         
-        if "title" in metadata:
-            st.subheader("Title")
-            st.write(metadata["title"])
+        with col1:
+            st.image(uploaded_file, caption="Uploaded Image.", use_column_width=True)
         
-        if "author" in metadata:
-            st.subheader("Author")
-            st.write(metadata["author"])
-        
-        if "description" in metadata:
-            st.subheader("Description")
-            st.write(metadata["description"])
-        
-        if "software" in metadata:
-            st.subheader("Software")
-            st.write(metadata["software"])
-
-    # メタデータを削除した画像をダウンロードボタンとして提供
-    image_bytes = remove_metadata_and_get_bytes(uploaded_file)
-    st.download_button("メタデータを削除した画像をダウンロード", image_bytes, file_name="image_without_metadata.png", mime="image/png")
-    
-    st.image(uploaded_file, caption="Uploaded Image.", use_column_width=True)
-    
-    st.subheader("All Metadata")
-    st.write(metadata)
+        with col2:
+            if 'parameters' in metadata:
+                st.subheader("Parameters for " + uploaded_file.name)
+                parameters = metadata['parameters']
+                st.text_area("", value=parameters, height=200, max_chars=None)
