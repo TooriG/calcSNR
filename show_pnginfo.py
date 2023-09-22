18c5a68d7284bccb63ecb537143896d1c6d6b2b0
TooriG <97956577+TooriG@users.noreply.github.com>
2023-09-22 13:01:38 +0900

    Update show_pnginfo.py

--- show_pnginfo.py
+++ show_pnginfo.py
@@ -26,34 +26,22 @@ st.title("PNG Metadata Extractor for Multiple Images")
 uploaded_files = st.file_uploader("複数のPNG画像をアップロードしてください", type=["png"], accept_multiple_files=True)
 
 if uploaded_files:
-    all_missing_metadata = True  # すべての画像にメタデータがないかどうかをチェックするフラグ
-
+    all_missing_metadata = True  # デフォルトでメタデータがないとする
     for uploaded_file in uploaded_files:
         metadata = extract_png_info(uploaded_file)
         if 'parameters' in metadata:
-            all_missing_metadata = False
-            break
+            all_missing_metadata = False  # メタデータが存在する画像が見つかった場合
 
     if all_missing_metadata:
-        st.error("すべての画像にメタデータが存在しません")
+        st.warning("すべての画像にメタデータが存在しません。")
 
-    # 一括ダウンロード用のZIPファイルを作成
-    zip_buffer = io.BytesIO()
-    with zipfile.ZipFile(zip_buffer, 'a', zipfile.ZIP_DEFLATED) as z:
-        for uploaded_file in uploaded_files:
-            image_bytes = remove_metadata_and_get_bytes(uploaded_file)
-            z.writestr(uploaded_file.name, image_bytes)
-    zip_buffer.seek(0)
-    st.download_button("メタデータを削除した画像を一括ダウンロード", zip_buffer, file_name="images_without_metadata.zip", mime="application/zip")
-    
-    # 画像とメタデータを6列で表示
-    for idx, uploaded_file in enumerate(uploaded_files):
-        cols = st.columns(6)
-        col = cols[idx % 6]
-        with col:
-            metadata = extract_png_info(uploaded_file)
-            col.image(uploaded_file, caption="Uploaded Image.", use_column_width=True)
-            
+    cols = st.columns(4)  # 4列を作成
+
+    for i, uploaded_file in enumerate(uploaded_files):
+        metadata = extract_png_info(uploaded_file)
+        with cols[i % 4]:
             if 'parameters' in metadata:
+                st.subheader("Parameters for " + uploaded_file.name)
                 parameters = metadata['parameters']
-                col.text_area("", value=parameters, height=200, max_chars=None)
+                st.text_area("", value=parameters, height=200, max_chars=None)
+            st.image(uploaded_file, caption="Uploaded Image.", use_column_width=True)
