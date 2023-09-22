6ec19347e180c19e013ff36b03b325102d96ed65
TooriG <97956577+TooriG@users.noreply.github.com>
2023-09-22 12:52:28 +0900

    Update show_pnginfo.py

--- show_pnginfo.py
+++ show_pnginfo.py
@@ -19,25 +19,35 @@ def remove_metadata_and_get_bytes(image_path):
         img_without_metadata.save(byte_io, format="PNG")
     return byte_io.getvalue()
 
-st.set_page_config(layout="wide")  # UIの横幅を広く取る
+st.set_page_config(layout="wide")
 
 st.title("PNG Metadata Extractor for Multiple Images")
 
 uploaded_files = st.file_uploader("複数のPNG画像をアップロードしてください", type=["png"], accept_multiple_files=True)
 
+metadata_exists = False
 if uploaded_files:
-    # 一括ダウンロード用のZIPファイルを作成
-    zip_buffer = io.BytesIO()
-    with zipfile.ZipFile(zip_buffer, 'a', zipfile.ZIP_DEFLATED) as z:
-        for uploaded_file in uploaded_files:
-            image_bytes = remove_metadata_and_get_bytes(uploaded_file)
-            z.writestr(uploaded_file.name, image_bytes)
-    zip_buffer.seek(0)
-    st.download_button("メタデータを削除した画像を一括ダウンロード", zip_buffer, file_name="images_without_metadata.zip", mime="application/zip")
+    for uploaded_file in uploaded_files:
+        metadata = extract_png_info(uploaded_file)
+        if 'parameters' in metadata:
+            metadata_exists = True
+            break
+
+    if not metadata_exists:
+        st.error("すべての画像にメタデータが存在しません")
+
+    else:
+        zip_buffer = io.BytesIO()
+        with zipfile.ZipFile(zip_buffer, 'a', zipfile.ZIP_DEFLATED) as z:
+            for uploaded_file in uploaded_files:
+                image_bytes = remove_metadata_and_get_bytes(uploaded_file)
+                z.writestr(uploaded_file.name, image_bytes)
+        zip_buffer.seek(0)
+        st.download_button("メタデータを削除した画像を一括ダウンロード", zip_buffer, file_name="images_without_metadata.zip", mime="application/zip")
     
     for uploaded_file in uploaded_files:
         metadata = extract_png_info(uploaded_file)
-        col1, col2 = st.columns([1, 2])
+        col1, col2 = st.columns(2)
         
         with col1:
             st.image(uploaded_file, caption="Uploaded Image.", use_column_width=True)
@@ -47,3 +57,5 @@ if uploaded_files:
                 st.subheader("Parameters for " + uploaded_file.name)
                 parameters = metadata['parameters']
                 st.text_area("", value=parameters, height=200, max_chars=None)
+            else:
+                st.write("この画像にはメタデータが存在しません。")
