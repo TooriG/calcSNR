96c5801e8b7eebb29386785c4d502cfb4c2a98ce
TooriG <97956577+TooriG@users.noreply.github.com>
2023-09-22 13:10:51 +0900

    Update show_pnginfo.py

--- show_pnginfo.py
+++ show_pnginfo.py
@@ -20,7 +20,7 @@ def remove_metadata_and_get_bytes(image_path):
     return byte_io.getvalue()
 
 def extract_prompt(parameters):
-    """parametersからpromptを抽出する関数"""
+    """parametersからprompt部分を抽出する関数"""
     return parameters.split("Negative prompt:")[0].strip()
 
 st.set_page_config(layout="wide")
@@ -29,19 +29,27 @@ st.title("PNG Metadata Extractor for Multiple Images")
 
 uploaded_files = st.file_uploader("複数のPNG画像をアップロードしてください", type=["png"], accept_multiple_files=True)
 
-all_metadata_present = True
-all_metadata = []
-
 if uploaded_files:
-    for uploaded_file in uploaded_files:
+    all_files_have_metadata = all(extract_png_info(file) for file in uploaded_files)
+
+    if not all_files_have_metadata:
+        st.warning("すべての画像にメタデータが存在しません")
+
+    for index, uploaded_file in enumerate(uploaded_files):
         metadata = extract_png_info(uploaded_file)
-        if 'parameters' not in metadata:
-            all_metadata_present = False
-        all_metadata.append(metadata)
+        
+        if index % 5 == 0:  # 5列ごとに新しい行を開始
+            cols = st.columns(5)
 
-    if not all_metadata_present:
-        st.error("すべての画像にメタデータが存在しません")
+        with cols[index % 5]:
+            st.image(uploaded_file, caption="Uploaded Image.", use_column_width=True)
+            
+            if 'parameters' in metadata:
+                prompt = extract_prompt(metadata['parameters'])
+                st.text_area("Prompt", value=prompt, height=100)
+                st.text_area("Parameters", value=metadata['parameters'], height=200)
 
+    # 一括ダウンロードボタン
     zip_buffer = io.BytesIO()
     with zipfile.ZipFile(zip_buffer, 'a', zipfile.ZIP_DEFLATED) as z:
         for uploaded_file in uploaded_files:
@@ -49,14 +57,3 @@ if uploaded_files:
             z.writestr(uploaded_file.name, image_bytes)
     zip_buffer.seek(0)
     st.download_button("メタデータを削除した画像を一括ダウンロード", zip_buffer, file_name="images_without_metadata.zip", mime="application/zip")
-
-    for uploaded_file, metadata in zip(uploaded_files, all_metadata):
-        cols = st.columns(5)
-        with cols[0]:
-            st.image(uploaded_file, caption="Uploaded Image.", use_column_width=True)
-        if 'parameters' in metadata:
-            prompt = extract_prompt(metadata['parameters'])
-            with cols[1]:
-                st.text_area("Prompt:", value=prompt, height=100)
-            with cols[2:5]:
-                st.text_area("Parameters:", value=metadata['parameters'], height=200)
