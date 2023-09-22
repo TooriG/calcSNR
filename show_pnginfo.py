4905d608bba09507e7ea3ab74e5662e23b556ee3
TooriG <97956577+TooriG@users.noreply.github.com>
2023-09-22 13:06:51 +0900

    Update show_pnginfo.py

--- show_pnginfo.py
+++ show_pnginfo.py
@@ -19,29 +19,44 @@ def remove_metadata_and_get_bytes(image_path):
         img_without_metadata.save(byte_io, format="PNG")
     return byte_io.getvalue()
 
+def extract_prompt(parameters):
+    """parametersからpromptを抽出する関数"""
+    return parameters.split("Negative prompt:")[0].strip()
+
 st.set_page_config(layout="wide")
 
 st.title("PNG Metadata Extractor for Multiple Images")
 
 uploaded_files = st.file_uploader("複数のPNG画像をアップロードしてください", type=["png"], accept_multiple_files=True)
 
+all_metadata_present = True
+all_metadata = []
+
 if uploaded_files:
-    all_missing_metadata = True  # デフォルトでメタデータがないとする
     for uploaded_file in uploaded_files:
         metadata = extract_png_info(uploaded_file)
-        if 'parameters' in metadata:
-            all_missing_metadata = False  # メタデータが存在する画像が見つかった場合
+        if 'parameters' not in metadata:
+            all_metadata_present = False
+        all_metadata.append(metadata)
 
-    if all_missing_metadata:
-        st.warning("すべての画像にメタデータが存在しません。")
+    if not all_metadata_present:
+        st.error("すべての画像にメタデータが存在しません")
 
-    cols = st.columns(4)  # 4列を作成
+    zip_buffer = io.BytesIO()
+    with zipfile.ZipFile(zip_buffer, 'a', zipfile.ZIP_DEFLATED) as z:
+        for uploaded_file in uploaded_files:
+            image_bytes = remove_metadata_and_get_bytes(uploaded_file)
+            z.writestr(uploaded_file.name, image_bytes)
+    zip_buffer.seek(0)
+    st.download_button("メタデータを削除した画像を一括ダウンロード", zip_buffer, file_name="images_without_metadata.zip", mime="application/zip")
 
-    for i, uploaded_file in enumerate(uploaded_files):
-        metadata = extract_png_info(uploaded_file)
-        with cols[i % 4]:
-            if 'parameters' in metadata:
-                st.subheader("Parameters for " + uploaded_file.name)
-                parameters = metadata['parameters']
-                st.text_area("", value=parameters, height=200, max_chars=None)
+    for uploaded_file, metadata in zip(uploaded_files, all_metadata):
+        cols = st.columns(5)
+        with cols[0]:
             st.image(uploaded_file, caption="Uploaded Image.", use_column_width=True)
+        if 'parameters' in metadata:
+            prompt = extract_prompt(metadata['parameters'])
+            with cols[1]:
+                st.text_area("Prompt:", value=prompt, height=100)
+            with cols[2:5]:
+                st.text_area("Parameters:", value=metadata['parameters'], height=200)
