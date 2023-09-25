0b972a2275c892cd9925ad00515e160b56f3b038
TooriG <97956577+TooriG@users.noreply.github.com>
2023-09-25 21:35:13 +0900

    Update show_pnginfo.py

--- show_pnginfo.py
+++ show_pnginfo.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from PIL import Image
 import io
-import zipfile
 
 def extract_png_info(image_path):
     """PNG画像からメタデータを抽出する関数"""
@@ -9,50 +8,40 @@ def extract_png_info(image_path):
         info = img.info
     return info
 
-def remove_metadata_and_get_bytes(image):
+def remove_metadata_and_get_bytes(image_path):
     """画像からメタデータを削除して、バイトデータを返す関数"""
-    data = list(image.getdata())
-    img_without_metadata = Image.new(image.mode, image.size)
-    img_without_metadata.putdata(data)
+    with Image.open(image_path) as img:
+        # メタデータを削除
+        data = list(img.getdata())
+        img_without_metadata = Image.new(img.mode, img.size)
+        img_without_metadata.putdata(data)
         
-    # 画像をバイトデータとして保存
-    byte_io = io.BytesIO()
-    img_without_metadata.save(byte_io, format="PNG")
+        # 画像をバイトデータとして保存
+        byte_io = io.BytesIO()
+        img_without_metadata.save(byte_io, format="PNG")
     return byte_io.getvalue()
-
-st.set_page_config(layout="wide")  # UIの横幅を広く取る
 
 st.title("PNG Metadata Extractor")
 
-uploaded_files = st.file_uploader("PNG画像をアップロードしてください", type=["png"], accept_multiple_files=True)
+uploaded_file = st.file_uploader("PNG画像をアップロードしてください", type=["png"])
 
-if uploaded_files:
-    zipped_file = io.BytesIO()
-    with zipfile.ZipFile(zipped_file, 'a', zipfile.ZIP_DEFLATED) as zf:
-        for uploaded_file in uploaded_files:
-            file_name = uploaded_file.name
-            img_data = remove_metadata_and_get_bytes(Image.open(uploaded_file))
-            zf.writestr(file_name, img_data)
-    zipped_file.seek(0)
+if uploaded_file:
+    with st.spinner("メタデータを抽出中..."):
+        metadata = extract_png_info(uploaded_file)
     
-    st.download_button(
-        "メタデータを削除した画像を一括ダウンロード",
-        zipped_file,
-        file_name="images_without_metadata.zip",
-        mime="application/zip"
-    )
+    if 'parameters' in metadata:
+        st.subheader("Parameters")
+        parameters = metadata['parameters']
+        st.markdown(f"```\n{parameters}\n```")
+    
+    st.subheader("PNG Info")
+    for key, value in metadata.items():
+        if key != 'parameters':
+            st.write(f"{key}: {value}")
 
-    for uploaded_file in uploaded_files:
-        with st.spinner(f"{uploaded_file.name}のメタデータを抽出中..."):
-            metadata = extract_png_info(uploaded_file)
-
-        col1, col2 = st.columns([1, 2])  # 画像とメタデータのカラム
-
-        with col1:
-            st.image(uploaded_file, caption=uploaded_file.name, use_column_width=True)
-
-        with col2:
-            if 'parameters' in metadata:
-                st.subheader(f"{uploaded_file.name} - Parameters")
-                parameters = metadata['parameters']
-                st.text_area("", value=parameters, height=300)
+    st.subheader("Uploaded Image")
+    st.image(uploaded_file, caption="Uploaded Image.", use_column_width=True)
+    
+    # メタデータを削除した画像をダウンロードボタンとして提供
+    image_bytes = remove_metadata_and_get_bytes(uploaded_file)
+    st.download_button("メタデータを削除した画像をダウンロード", image_bytes, file_name="image_without_metadata.png", mime="image/png")
