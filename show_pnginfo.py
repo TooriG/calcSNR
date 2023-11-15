c98d4665d37b7231f391767bed75c9f46f6c0d7f
TooriG <97956577+TooriG@users.noreply.github.com>
2023-11-15 23:25:35 +0900

    Update show_pnginfo.py

--- show_pnginfo.py
+++ show_pnginfo.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from PIL import Image
 
-st.title('PNG Metadata Viewer')
+st.title('PNG Parameters Metadata Viewer')
 
 # ファイルアップローダーを作成
 uploaded_files = st.file_uploader("PNG画像をアップロードしてください", type=['png'], accept_multiple_files=True)
@@ -9,11 +9,11 @@ uploaded_files = st.file_uploader("PNG画像をアップロードしてくださ
 if uploaded_files:
     for uploaded_file in uploaded_files:
         with Image.open(uploaded_file) as img:
-            # メタデータの有無を確認
-            if img.info:
-                st.subheader(f"{uploaded_file.name}のメタデータ")
-                for key, value in img.info.items():
-                    st.text(f"{key}: {value}")
+            # 'parameters' メタデータの有無を確認
+            if "parameters" in img.info:
+                st.subheader(f"{uploaded_file.name}の 'parameters' メタデータ")
+                # テキストボックスにメタデータを表示
+                st.text_area("メタデータ内容", img.info["parameters"], height=150)
             else:
-                # メタデータがない場合
-                st.text(f"{uploaded_file.name}にメタデータはありません。")
+                # 'parameters' メタデータがない場合
+                st.text(f"{uploaded_file.name}に 'parameters' メタデータはありません。")
