910880ff70e8b1a8dfbcecdc7077262db04c4616
TooriG <97956577+TooriG@users.noreply.github.com>
2023-11-28 15:16:49 +0900

    Update image2x.py

--- image2x.py
+++ image2x.py
@@ -3,44 +3,37 @@ from PIL import Image
 import io
 import zipfile
 
-# Function to resize the image
+# 画像をリサイズする関数
 def resize_image(image):
     original_image = Image.open(image)
     size = (original_image.width * 2, original_image.height * 2)
     resized_image = original_image.resize(size, Image.Resampling.LANCZOS)
     return resized_image
 
-# Streamlit application interface
-st.title('Image Resizer Web App')
+# Streamlit UI
+st.title('画像リサイズアプリ')
 
-# Multiple file uploader
-uploaded_files = st.file_uploader("Upload images", accept_multiple_files=True, type=['jpg', 'jpeg', 'png'])
+uploaded_files = st.file_uploader("画像をアップロードしてください", type=['jpg', 'png'], accept_multiple_files=True)
 
 if uploaded_files:
-    # In-memory buffer for the zip file
+    # Zipファイルの作成
     zip_buffer = io.BytesIO()
-
-    # Create a zip file
     with zipfile.ZipFile(zip_buffer, 'w', zipfile.ZIP_DEFLATED) as zip_file:
         for uploaded_file in uploaded_files:
-            # Resize the image
+            # 画像をリサイズ
             image = resize_image(uploaded_file)
-
-            # In-memory buffer for the resized image
+            # メモリ内のバッファに画像を保存
             img_buffer = io.BytesIO()
-            image.save(img_buffer, format=image.format)
+            image.save(img_buffer, format='PNG')
             img_buffer.seek(0)
+            # Zipファイルに追加
+            zip_file.writestr(uploaded_file.name, img_buffer.read())
 
-            # Add the image to the zip file
-            zip_file.writestr(uploaded_file.name, img_buffer.getvalue())
-
-    # Reset the buffer's cursor to the beginning
+    # ユーザーにダウンロード用のリンクを提供
     zip_buffer.seek(0)
-
-    # Download button
     st.download_button(
-        label="Download Resized Images",
+        label="画像をダウンロード",
         data=zip_buffer,
         file_name="resized_images.zip",
-        mime='application/zip'
+        mime="application/zip"
     )
