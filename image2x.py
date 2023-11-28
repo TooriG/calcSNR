3b09bf859d351ec5dfaae9c704fedc133e3957ec
TooriG <97956577+TooriG@users.noreply.github.com>
2023-11-28 15:13:12 +0900

    Update image2x.py

--- image2x.py
+++ image2x.py
@@ -3,36 +3,44 @@ from PIL import Image
 import io
 import zipfile
 
-# 画像をリサイズする関数
+# Function to resize the image
 def resize_image(image):
     original_image = Image.open(image)
     size = (original_image.width * 2, original_image.height * 2)
-    resized_image = original_image.resize(size, Image.ANTIALIAS)
+    resized_image = original_image.resize(size, Image.Resampling.LANCZOS)
     return resized_image
 
-# Streamlit UI
-st.title('画像リサイズアプリ')
+# Streamlit application interface
+st.title('Image Resizer Web App')
 
-uploaded_files = st.file_uploader("画像をアップロードしてください", type=['jpg', 'png'], accept_multiple_files=True)
+# Multiple file uploader
+uploaded_files = st.file_uploader("Upload images", accept_multiple_files=True, type=['jpg', 'jpeg', 'png'])
 
 if uploaded_files:
-    # Zipファイルの作成
+    # In-memory buffer for the zip file
     zip_buffer = io.BytesIO()
-    with zipfile.ZipFile(zip_buffer, 'a', zipfile.ZIP_DEFLATED, False) as zip_file:
-        for uploaded_file in uploaded_files:
-            # 画像をリサイズ
-            image = resize_image(uploaded_file)
-            # メモリ内のバッファに画像を保存
-            img_buffer = io.BytesIO()
-            image.save(img_buffer, format='PNG')
-            img_buffer.seek(0)
-            # Zipファイルに追加
-            zip_file.writestr(uploaded_file.name, img_buffer.read())
 
-    # ユーザーにダウンロード用のリンクを提供
+    # Create a zip file
+    with zipfile.ZipFile(zip_buffer, 'w', zipfile.ZIP_DEFLATED) as zip_file:
+        for uploaded_file in uploaded_files:
+            # Resize the image
+            image = resize_image(uploaded_file)
+
+            # In-memory buffer for the resized image
+            img_buffer = io.BytesIO()
+            image.save(img_buffer, format=image.format)
+            img_buffer.seek(0)
+
+            # Add the image to the zip file
+            zip_file.writestr(uploaded_file.name, img_buffer.getvalue())
+
+    # Reset the buffer's cursor to the beginning
+    zip_buffer.seek(0)
+
+    # Download button
     st.download_button(
-        label="画像をダウンロード",
+        label="Download Resized Images",
         data=zip_buffer,
         file_name="resized_images.zip",
-        mime="application/zip"
+        mime='application/zip'
     )
