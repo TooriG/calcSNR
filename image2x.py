0b2881f93b4c87cde99a946e734b2b75f7b64111
TooriG <97956577+TooriG@users.noreply.github.com>
2023-11-30 23:34:50 +0900

    Update image2x.py

--- image2x.py
+++ image2x.py
@@ -1,34 +1,41 @@
 import streamlit as st
 from PIL import Image
-import io
 import zipfile
+import io
+import os
 
 def resize_image(image_path):
     with Image.open(image_path) as img:
+        # Resize the image to twice its size
         img = img.resize((img.width * 2, img.height * 2), Image.Resampling.LANCZOS)
         img_byte_arr = io.BytesIO()
         img.save(img_byte_arr, format=img.format)
         img_byte_arr = img_byte_arr.getvalue()
     return img_byte_arr
 
-st.title('Folder Image Resizer')
+st.title('Folder/Image Resizer')
 
-uploaded_file = st.file_uploader("Upload a ZIP file containing images", type=['zip'])
+# File uploader for ZIP files
+uploaded_file = st.file_uploader("Upload a ZIP file", type=['zip'])
 
 if uploaded_file:
     zip_buffer = io.BytesIO()
     with zipfile.ZipFile(zip_buffer, 'w', zipfile.ZIP_DEFLATED) as zip_out:
         with zipfile.ZipFile(uploaded_file, 'r') as zip_in:
             for file in zip_in.namelist():
+                # Check if the file is an image
                 if file.lower().endswith(('.png', '.jpg', '.jpeg')):
                     with zip_in.open(file) as img_file:
+                        # Resize image and get byte data
                         img_data = resize_image(img_file)
+                        # Write the resized image to the output ZIP
                         zip_out.writestr(file, img_data)
 
+    # Reset buffer position and create download button
     zip_buffer.seek(0)
     st.download_button(
         label="Download Resized Images",
         data=zip_buffer,
-        file_name="resized_folder_images.zip",
+        file_name="resized_images.zip",
         mime="application/zip"
     )
