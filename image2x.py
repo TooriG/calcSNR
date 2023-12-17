cd2e70724e1cb4a99948a235702de6baf024ced8
TooriG <97956577+TooriG@users.noreply.github.com>
2023-12-17 15:06:50 +0900

    Update image2x.py

--- image2x.py
+++ image2x.py
@@ -4,16 +4,28 @@ import io
 import zipfile
 
 # 画像をリサイズする関数
-def resize_image(image):
-    original_image = Image.open(image)
-    # PNG metadataのコピー（PngInfoオブジェクトを使用）
-    metadata = PngImagePlugin.PngInfo()
-    if original_image.format == 'PNG':
-        for k, v in original_image.info.items():
-            metadata.add_text(k, v)
-    size = (original_image.width * 2, original_image.height * 2)
-    resized_image = original_image.resize(size, Image.Resampling.LANCZOS)
-    return resized_image, metadata
+def resize_image(image_file):
+    with Image.open(image_file) as original_image:
+        # PNG metadataのコピー（PngInfoオブジェクトを使用）
+        metadata = PngImagePlugin.PngInfo()
+        if original_image.format == 'PNG':
+            for k, v in original_image.info.items():
+                metadata.add_text(k, v)
+
+        size = (original_image.width * 2, original_image.height * 2)
+        resized_image = original_image.resize(size, Image.Resampling.LANCZOS)
+
+        # メモリ内のバッファに画像を保存
+        img_buffer = io.BytesIO()
+        if resized_image.format == 'PNG':
+            # PNG metadataを保持しながら保存
+            resized_image.save(img_buffer, format='PNG', pnginfo=metadata)
+        else:
+            # 他のフォーマットの場合はフォーマットを指定して保存
+            resized_image.save(img_buffer, format=resized_image.format)
+        img_buffer.seek(0)
+
+    return img_buffer
 
 # Streamlit UI
 st.title('画像リサイズアプリ')
@@ -25,17 +37,7 @@ if uploaded_files:
     zip_buffer = io.BytesIO()
     with zipfile.ZipFile(zip_buffer, 'w', zipfile.ZIP_DEFLATED) as zip_file:
         for uploaded_file in uploaded_files:
-            # 画像をリサイズ
-            image, metadata = resize_image(uploaded_file)
-            # メモリ内のバッファに画像を保存
-            img_buffer = io.BytesIO()
-            if image.format == 'PNG':
-                # PNG metadataを保持しながら保存
-                image.save(img_buffer, format='PNG', pnginfo=metadata)
-            else:
-                # 他のフォーマットの場合はフォーマットを指定して保存
-                image.save(img_buffer, format='JPEG')
-            img_buffer.seek(0)
+            img_buffer = resize_image(uploaded_file)
             # Zipファイルに追加
             zip_file.writestr(uploaded_file.name, img_buffer.read())
 
