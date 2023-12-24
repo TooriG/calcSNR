6912ddfc9626584e163abc870dcfbaff359661ee
TooriG <97956577+TooriG@users.noreply.github.com>
2023-12-24 14:04:04 +0900

    Update calcSNR.py

--- calcSNR.py
+++ calcSNR.py
@@ -3,47 +3,51 @@ from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 
-# 画像のノイズ特性を計算する関数
-def analyze_image(image):
-    # 画像をグレースケールに変換し、NumPy配列にする
-    gray_image = np.array(image.convert('L'))
+# RGBチャネルのヒストグラムを計算する関数
+def calculate_histograms(image):
+    # NumPy配列に変換
+    image_array = np.array(image)
 
-    # 平均と標準偏差を計算
-    mean = np.mean(gray_image)
-    std = np.std(gray_image)
+    # RGBチャネルごとのヒストグラムを計算
+    red_hist, bins = np.histogram(image_array[:,:,0], bins=256, range=[0,256])
+    green_hist, _ = np.histogram(image_array[:,:,1], bins=256, range=[0,256])
+    blue_hist, _ = np.histogram(image_array[:,:,2], bins=256, range=[0,256])
+    
+    return red_hist, green_hist, blue_hist, bins
 
-    # ヒストグラムを計算（256階調）
-    histogram, _ = np.histogram(gray_image, bins=256)
+# ヒストグラムを表示する関数
+def plot_histograms(red_hist, green_hist, blue_hist, bins):
+    plt.figure(figsize=(10, 4))
+    plt.title('Color Histogram')
+    plt.xlabel('Pixel Value')
+    plt.ylabel('Frequency')
 
-    return mean, std, histogram
+    plt.bar(bins[:-1] - 0.75, red_hist, color='red', alpha=0.6, label='Red', width=0.5)
+    plt.bar(bins[:-1] - 0.25, green_hist, color='green', alpha=0.6, label='Green', width=0.5)
+    plt.bar(bins[:-1] + 0.25, blue_hist, color='blue', alpha=0.6, label='Blue', width=0.5)
+    
+    plt.legend()
+    return plt
 
 # Streamlitアプリケーションのメイン関数
 def main():
-    st.title('Image Analysis App')
+    st.title('Color Image Histogram Analysis')
 
     # 複数の画像のアップロード
-    uploaded_files = st.file_uploader("Please upload images", type=["jpg", "png"], accept_multiple_files=True)
+    uploaded_files = st.file_uploader("Please upload color images", type=["jpg", "png"], accept_multiple_files=True)
     
     for uploaded_file in uploaded_files:
-        # カラムを作成して、画像と分析結果を横に表示
+        # カラムを作成して、画像とヒストグラムを横に表示
         col1, col2 = st.columns(2)
         
         with col1:  # 画像の表示
             image = Image.open(uploaded_file)
             st.image(image, caption=f'Uploaded Image: {uploaded_file.name}', use_column_width=True)
         
-        with col2:  # 画像分析と結果の表示
-            mean, std, histogram = analyze_image(image)
-            st.write(f"Mean: {mean:.2f}")
-            st.write(f"Standard Deviation: {std:.2f}")
-            
-            # ヒストグラムを表示
-            fig, ax = plt.subplots()
-            ax.bar(range(256), histogram, color='gray')
-            ax.set_title('Image Histogram')
-            ax.set_xlabel('Pixel Value')
-            ax.set_ylabel('Frequency')
-            st.pyplot(fig)
+        with col2:  # ヒストグラムの表示
+            red_hist, green_hist, blue_hist, bins = calculate_histograms(image)
+            plt = plot_histograms(red_hist, green_hist, blue_hist, bins)
+            st.pyplot(plt)
 
 if __name__ == "__main__":
     main()
